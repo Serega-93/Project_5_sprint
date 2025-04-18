fb6e5fae890d6d8f31dec44e10c8dee2d6009eb1
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-04-18 14:24:14 +0500

    Created tests test_log_in_account

--- locators.py
+++ locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class Locators:
 
     PERSONAL_ACCOUNT_BUTTON = By.XPATH, "//p[text()='Личный Кабинет']" # кнопка 'Личный кабинет'
@@ -8,8 +9,9 @@ class Locators:
     RESTORE_BUTTON = By.XPATH, "//button[text()='Восстановить']" # кнопка 'Восстановить'
     LOGIN_BUTTON = By.XPATH, '//button[text()="Войти"]' # кнопка 'Войти'
     NAME_FIELD = By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input' # поле 'имя'
-    EMAIL_FIELD = By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input' # поле 'email'
+    EMAIL_FIELD_REG = By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[2]/div/div/input' # поле 'email'(регистрация)
     PASSWORD_FIELD = By.XPATH, '//input[@name="Пароль"]' # поле 'пароль'
+    EMAIL_FIELD_AUTH = By.XPATH, '//*[@id="root"]/div/main/div/form/fieldset[1]/div/div/input' # поле 'email'(авторизация)
     REGISTER_SECTION = By.XPATH, '//a[@class="Auth_link__1fOlj" and text()="Зарегистрироваться"]' # раздел 'зарегистрироваться'
     RESTORE_PASSWORD_SECTION = By.XPATH, '//a[@class="Auth_link__1fOlj" and text()="Восстановить пароль"]' # раздел 'восстановить пароль'
     CONSTRUCTOR_SECTION = By.XPATH, '//p[text()="Конструктор"]' # раздел 'Конструктор'
@@ -21,3 +23,4 @@ class Locators:
     ERROR_PASSWORD = By.XPATH, '// p[@class ="input__error text_type_main-default"]' # ошибка 'Некорректный пароль'
     SAVE_BUTTON = By.XPATH, '//button[text()="Сохранить"]' # кнопка 'Сохранить' в личном кабинете
     PLACE_ORDER_BUTTON = By.XPATH, '// button[text() = "Оформить заказ"]' # кнопка 'Оформить заказ'
+    LOGIN_LINKS = By.XPATH, '//a[@class ="Auth_link__1fOlj"]' # ссылка 'Войти'
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -14,13 +14,27 @@ class TestRegistrationWithNewData:
         driver.find_element(*Locators.REGISTER_SECTION).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.REGISTER_BUTTON))
         driver.find_element(*Locators.NAME_FIELD).send_keys(name)
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(email)
+        driver.find_element(*Locators.EMAIL_FIELD_REG).send_keys(email)
         driver.find_element(*Locators.PASSWORD_FIELD).send_keys(password)
         driver.find_element(*Locators.REGISTER_BUTTON).click()
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
-        driver.find_element(*Locators.NAME_FIELD).send_keys(email)
+        driver.find_element(*Locators.EMAIL_FIELD_AUTH).send_keys(email)
         driver.find_element(*Locators.PASSWORD_FIELD).send_keys(password)
         driver.find_element(*Locators.LOGIN_BUTTON).click()
         text = WebDriverWait(driver, 5).until(EC.visibility_of_element_located
                                                          (Locators.PLACE_ORDER_BUTTON)).text
         assert text == 'Оформить заказ' # кнопка 'Оформить заказ' доступна только зарегистрированному пользователю
+
+    def test_invalid_password_error(self, driver):
+        name, email, password = generate_registration_data()
+        driver.find_element(*Locators.SIGN_ACCOUNT_BUTTON).click()
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.REGISTER_SECTION))
+        driver.find_element(*Locators.REGISTER_SECTION).click()
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.REGISTER_BUTTON))
+        driver.find_element(*Locators.NAME_FIELD).send_keys(name)
+        driver.find_element(*Locators.EMAIL_FIELD_REG).send_keys(email)
+        driver.find_element(*Locators.PASSWORD_FIELD).send_keys('1')
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+        text = WebDriverWait(driver, 5).until(EC.visibility_of_element_located
+                                              (Locators.ERROR_PASSWORD)).text
+        assert text == 'Некорректный пароль'
